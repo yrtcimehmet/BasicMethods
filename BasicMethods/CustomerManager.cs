5030d056c2b89ae2f6e86eb3ae81a2653ede47ef
Mehmet Yirtici <yrtci.mehmet@gmail.com>
2022-10-09 01:53:39 +0300

    Added Customer Class and CustomerManager Class

---
+++ BasicMethods/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMethods
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added: " + customer.Name + "Customer Id : " + customer.Id);
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine("Customer with id " + customer.Id+ " has Been Removed" );
+        }
+
+        public void list(Customer customer)
+        {
+            Console.WriteLine("Customer Name : " +customer.Name + " Customer Id: " + customer.Id);
+        }
+    }
+}
--- BasicMethods/Program.cs
+++ BasicMethods/Program.cs
@@ -4,6 +4,30 @@
     {
         static void Main(string[] args)
         {
+            Customer James = new Customer();
+            James.Id = 283663;
+            James.Name = "James";
+
+            Customer Elizabeth = new Customer();
+            Elizabeth.Id = 792548;
+            Elizabeth.Name = "Elizabeth";
+
+            Customer David = new Customer();
+            David.Id = 525521;
+            David.Name = "David";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(James);
+            customerManager.Add(Elizabeth);
+            customerManager.Add(David);
+
+            customerManager.Remove(James);
+
+            customerManager.list(David);
+
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
